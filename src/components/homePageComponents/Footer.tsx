5f6bf58cb2b6aaf1442f257553a100ae3113c209
Polina <polmi713@gmail.com>
2021-10-11 18:23:13 +0300

    correct styles for footer components add footer components to home page

--- src/components/homePageComponents/Footer.tsx
+++ src/components/homePageComponents/Footer.tsx
@@ -5,44 +5,45 @@ import {Container, Row, Col, Image} from 'react-bootstrap'
 
 const Footer = () => {
   return (
-    <Container fluid style={{backgroundColor: '#1e3d59', color: '#f5f0e1', padding: '20px 60px', fontSize: '14px'}}>
+    <Container fluid style={{backgroundColor: '#000', color: 'rgba(255, 255, 255, 0.55)', padding: '20px 60px', fontSize: '14px'}}>
       <Row style={{display: 'flex'}}>
         <Col style={{width: '50%'}}>
-          <Container style={{display: 'flex', flexDirection: 'column'}}>
-            <Row style={{fontWeight: 'bold', fontSize: '18px', color: '#ff6e40', marginBottom: '10px'}}>
-              <Image src='../logo.png' />
-              <span>OceanBar</span>
+          <Container style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', padding: '0'}}>
+            <Row style={{fontWeight: 'bold', fontSize: '18px', color: '#ff9e05', marginBottom: '10px', display: 'flex'}}>
+              <Col>
+                <Image src='seafood.png' alt='logo' width={25} height={25} />
+              </Col>
+              <Col style={{padding: '0'}}>
+                <span style={{fontWeight: 'bold', fontSize: '18px', color: '#ff9e05', marginBottom: '10px', display: 'flex'}}>OceanBar</span>
+              </Col>
             </Row>
-            <Row style={{display: 'flex'}}>
-              <Col style={{display: 'flex', flexDirection: 'column'}}>
+            <Row style={{display: 'flex', whiteSpace: 'nowrap'}}>
+              <Col style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
                 <span>Меню</span>
                 <span>Забронировать стол</span>
                 <span>Оформить заказ</span>
                 <span>Корзина</span>
                 <span>Профиль</span>
               </Col>
-              <Col style={{display: 'flex', flexDirection: 'column', marginLeft: '25px'}}>
+              <Col style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginLeft: '30px', textAlign: 'left'}}>
                 <span>Условия пользования</span>
                 <span>Политика конфиденциальности</span>
                 <span>Политика cookies</span>
               </Col>
             </Row>
-          </Container>
-
-          <Container style={{fontSize: '10px', marginTop: '20px'}}>
-            <Row>
+            <Row style={{fontSize: '10px', marginTop: '20px'}}>
               <span>© All rights reserved | OceanBar 2021</span>
             </Row>
           </Container>
         </Col>
 
         <Col style={{width: '50%'}}>
-          <Container style={{display: 'flex', flexDirection: 'column'}}>
-            <Row style={{fontWeight: 'bold', fontSize: '18px', color: '#ff6e40', marginBottom: '10px'}}>
-              Мы в социальных сетях
+          <Container style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+            <Row style={{fontWeight: 'bold', fontSize: '18px', color: '#ff9e05', marginBottom: '10px'}}>
+              <span>Мы в социальных сетях</span>
             </Row>
             <Row style={{display: 'flex'}}>
-              <Col style={{display: 'flex', flexDirection: 'column'}}>
+              <Col style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
                 <span>Facebook</span>
                 <span>Instagram</span>
                 <span>VK</span>
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import OceanBarHeader from '../components/homePageComponents/Header'
 import HowItWorks from '../components/homePageComponents/HowItWorks'
 import BookATable from '../components/homePageComponents/BookATable'
+import ContactsCard from '../components/homePageComponents/ContactsCard'
+import Footer from '../components/homePageComponents/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <OceanBarHeader />
       <HowItWorks />
       <BookATable />
+      <ContactsCard />
+      <Footer />
     </div>
   )
 }
