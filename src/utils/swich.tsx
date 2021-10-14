c0cb540cd799b555986abf08e0be7c2eabd3f616
Anna <anna.vl.gromova@gmail.com>
2021-10-14 21:04:36 +0300

    Switch moved to App.tsx

--- src/utils/swich.tsx
+++ src/utils/swich.tsx
@@ -8,10 +8,10 @@ import {
 import Menu from '../pages/menuPage/menuPage'
 import Booking from '../pages/bookingPage/bookingPage'
 import Login from '../pages/loginPage/loginPage'
-import Home from '../pages/homePage/homePage'
 import Page404 from '../pages/page404/page404'
 import Checkout from '../pages/orderPage/orderPage'
 import Cart from '../pages/cartPage/cartPage'
+import Home from '../pages/homePage/homePage'
 
 
 const SwitchPager = () => {
