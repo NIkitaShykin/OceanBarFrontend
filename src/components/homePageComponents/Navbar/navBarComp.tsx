b577e2562aab3bae14845521ffccc0cedcfdfb60
Anna <anna.vl.gromova@gmail.com>
2021-10-15 12:33:48 +0300

    style change for navbar

--- src/components/homePageComponents/Navbar/navBarComp.tsx
+++ src/components/homePageComponents/Navbar/navBarComp.tsx
@@ -6,59 +6,71 @@ import {Navbar,
   FormControl,
   Button} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
-import Image from 'react-bootstrap/Image'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faShoppingBasket, faUserCircle} from '@fortawesome/free-solid-svg-icons'
+import {faShoppingBasket,
+  faUser} from '@fortawesome/free-solid-svg-icons'
+import './navBar.scss'
 
 
 const NavBarComponent = () => {
   return (
     <div>
       <Navbar bg='dark' variant={'dark'} expand='lg'>
-        <Image src='images/logo.png' alt='logo'
-          className='mx-2'
-          width={50}
-          height={50}/>
-        <Navbar.Brand href='/'>Ocean Bar</Navbar.Brand>
+        <Navbar.Brand
+          href='/'
+          className='navbar-brand-mr'>Ocean Bar
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls='navbarScroll' />
         <Navbar.Collapse
+
           id='navbarScroll'
-          className='justify-content-start mr-2'>
+          className='justify-content-between mr-2'>
           <Nav
             className='mr-auto m-3 my-lg-0'
             navbarScroll
           >
-            <Nav.Link as={Link} to={'/menu'}>Меню
+            <Nav.Link
+              as={Link}
+              to={'/menu'}
+              className='nav-link-mr'>Меню
             </Nav.Link>
-            <Nav.Link as={Link} to={'/order'}>Оформить заказ
-            </Nav.Link>
-            <Nav.Link as={Link} to={'/booking-table'}>Забронировать стол
+            <Nav.Link
+              as={Link}
+              to={'/booking-table'}
+              className='nav-link-mr'>Забронировать стол
             </Nav.Link>
           </Nav>
-          <Form className='d-flex mx-5'>
-            <FormControl
-              type='search'
-              placeholder='Искать блюдо...'
-              className='mr-2'
-              aria-label='Search'
-            />
-            <Button variant='outline-warning'
-              className='mx-2'>Искать</Button>
-          </Form>
-          <Nav.Link as={Link} to={'/cart'}>
-            <FontAwesomeIcon
-              icon={faShoppingBasket}
-              className='mr-auto m-3 my-lg-0'
-              size='2x'
-              color='white'/>
-          </Nav.Link>
-          <Nav.Link as={Link} to={'/login'}>
-            <FontAwesomeIcon
-              icon={faUserCircle}
-              className='mr-auto m-3 my-lg-0'
-              size='2x'
-              color='white'/>
-          </Nav.Link>
+          <div
+            className='d-flex justify-content-end'>
+            <Form className='d-flex mx-6'>
+              <FormControl
+                type='search'
+                placeholder='Искать блюдо...'
+                className='mr-2'
+                aria-label='Search'
+              />
+              <Button variant='outline-warning'
+                className='mx-2'>Искать</Button>
+            </Form>
+            <Nav.Link as={Link} to={'/cart'}
+              className='nav-link-icon'>
+              <FontAwesomeIcon
+                icon={faShoppingBasket}
+                className='fas fa-shopping-basket mr-auto
+                 m-3 my-lg-0 icon-height
+               svg-inline--fa fa-w-18-24'
+                color='white'/>
+            </Nav.Link>
+            <Nav.Link as={Link} to={'/login'}
+              className='nav-link-icon'>
+              <FontAwesomeIcon
+                icon={faUser}
+                className='far fa-user mr-auto m-3 my-lg-0 icon-height
+              svg-inline--fa fa-w-18-24'
+                color='white'/>
+            </Nav.Link>
+          </div>
+
         </Navbar.Collapse>
       </Navbar>
     </div>
