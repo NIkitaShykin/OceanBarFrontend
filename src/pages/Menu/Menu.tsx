e14fa11265a5809fe38013d1274efa09d6c12e6d
Anna <anna.vl.gromova@gmail.com>
2021-10-15 18:19:27 +0300

    hashrouter bug fixed

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
 import {Col, Container, Row} from 'react-bootstrap'
-<<<<<<< refs/remotes/origin/page404
-import {BrowserRouter as Router} from 'react-router-dom'
-import MenuHeader from './MenuHeader'
-import MenuRoutes from './MenuRoutes'
-
-
-const Menu = () => {
-=======
 import MenuHeader from './MenuHeader'
 
 
 const Menu: React.FC = () => {
->>>>>>> hashrouter bug fixed
   return (
     <Container>
       <Row>
@@ -24,14 +15,7 @@ const Menu: React.FC = () => {
           </h1>
         </Col>
       </Row>
-<<<<<<< refs/remotes/origin/page404
-      
-        <MenuHeader/>
-        {/* <MenuRoutes /> */}
-     
-=======
       <MenuHeader/>
->>>>>>> hashrouter bug fixed
     </Container>
   )
 }
