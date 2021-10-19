36141ec7b110fc54614f643422a80043682ba9f2
Anna <anna.vl.gromova@gmail.com>
2021-10-19 12:00:38 +0300

    logo style change & dropdown margin add

--- src/pages/Menu/Assortment/DessertList.tsx
+++ src/pages/Menu/Assortment/DessertList.tsx
@@ -1,19 +1,21 @@
-import foodData from '../DB/foodData';
-import { Row, CardGroup } from 'react-bootstrap';
-import ListItem from './ListItem';
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable require-jsdoc */
+import foodData from '../DB/foodData'
+import {Row} from 'react-bootstrap'
+import ListItem from './ListItem'
 
 
-function DessertList() {
-
+function DessertList(): JSX.Element {
   return (
     <>
       <Row >
         <ListItem data={foodData[4]} />
       </Row>
     </>
-  );
+  )
 }
 
-export default DessertList;
+export default DessertList
+
 
 
