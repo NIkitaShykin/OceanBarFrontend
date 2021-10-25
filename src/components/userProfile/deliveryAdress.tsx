eb80191d804c4aafa63baf36724c51cf409e7933
Anna <anna.vl.gromova@gmail.com>
2021-10-25 09:02:09 +0300

    add profile layout

---
+++ src/components/userProfile/deliveryAdress.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const DeliveryAdress = () => {
+  return (
+    <div className='profile-block justify-content-start'>
+      <h2>Адрес доставки</h2>
+      <div className='info-block'>
+        <div className='locality'>г. Минск</div>
+        <div className='street'>ул. Улица</div>
+        <div className='building'>д. 10</div>
+        <div className='apartment'>кв. 3</div>
+        <div className='entrance-floor'>подъезд 3, этаж 3</div>
+      </div>
+
+      <button className='btn btn-outline-warning offset-md-10'>
+        Изменить
+      </button>
+    </div>
+  )
+}
+
+export default DeliveryAdress
