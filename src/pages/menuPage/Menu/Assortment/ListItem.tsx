61d1cba13cecc24fc6841e8b4380afdbc9ad3cdb
luchinayan <yanluchina48@gmail.com>
2021-10-29 17:38:28 +0300

    add dish to the cart functionality

--- src/pages/menuPage/Menu/Assortment/ListItem.tsx
+++ src/pages/menuPage/Menu/Assortment/ListItem.tsx
@@ -1,23 +1,34 @@
-import { NavLink, useParams } from 'react-router-dom'
-import { Button, Card, Col, Row } from 'react-bootstrap'
+import {NavLink, useParams} from 'react-router-dom'
+import {Button, Card, Col, Row} from 'react-bootstrap'
 import React from 'react'
+import {addDishToCart} from '../../../../redux/actions'
+import {useDispatch} from 'react-redux'
 
 function ListItem(props: any) {
-  const token = useParams<{ token: string }>()
-
-
+  const token = useParams<{token: string}>()
+  const dispatch = useDispatch()
+  const orderDish = (dish) => {
+    dispatch(
+      addDishToCart({
+        id: dish.id,
+        name: dish.name,
+        prise: dish.prise,
+        image: dish.image,
+      })
+    )
+    alert(` блюдо : '${dish.name}'  добавлено в корзину `)
+  }
 
   // @ts-ignore
   const arrayDishes = props.data.map((dish) => {
-
     return (
       <Col>
-        <Card      
+        <Card
           // bg='light'
           key={`${dish.id}`}
           text='dark'
           className='mb-3 mx-2 my-2'
-          style={props.isIntresting ? { width: '12rem' } : { width: '18rem' }}
+          style={props.isIntresting ? {width: '12rem'} : {width: '18rem'}}
         >
           <NavLink to={'/dish/' + dish.id}>
             <Card.Img
@@ -30,7 +41,9 @@ function ListItem(props: any) {
 
           <Card.Body>
             <Row>
-              <Col md="auto"><Card.Title>{dish.name}</Card.Title></Col>
+              <Col md='auto'>
+                <Card.Title>{dish.name}</Card.Title>
+              </Col>
               <Col sm={9}></Col>
             </Row>
 
@@ -42,31 +55,40 @@ function ListItem(props: any) {
 
             <Row>
               <Col xs={5} sm={5} md={5} lg={5}>
-                <div style={{ display: "flex" }}>
-                  <span style={{fontSize: "15px"}}><strong>{dish.prise}</strong></span>
-                  <span style={{fontSize: "12px", marginTop:"3px", marginLeft:"2px" }}>BYN</span>
-                  </div>
+                <div style={{display: 'flex'}}>
+                  <span style={{fontSize: '15px'}}>
+                    <strong>{dish.prise}</strong>
+                  </span>
+                  <span
+                    style={{
+                      fontSize: '12px',
+                      marginTop: '3px',
+                      marginLeft: '2px',
+                    }}
+                  >
+                    BYN
+                  </span>
+                </div>
               </Col>
-              <Col xs={2}  sm={3} md={3} lg={3}></Col>
+              <Col xs={2} sm={3} md={3} lg={3}></Col>
               <Col xs={5} sm={4} md={4} lg={4}>
                 {dish.weight}
               </Col>
             </Row>
 
-            <NavLink to={'/dish/' + dish.id}>
             <Button
-              variant="outline-warning"
-              onClick={() => console.log('открыть' + `${dish.id}`)}
-              id={dish.id}
-              style={props.isIntresting ? { display: 'none' } : {}}
+              variant='outline-warning'
+              onClick={() => {
+                orderDish(dish)
+              }}
+              style={props.isIntresting ? {display: 'none'} : {}}
             >
               Заказать
             </Button>
-            </NavLink>
             <br />
           </Card.Body>
         </Card>
-      </Col >
+      </Col>
     )
   })
 
--- src/redux/reducers/cartReducer.tsx
+++ src/redux/reducers/cartReducer.tsx
@@ -3,36 +3,26 @@ import * as R from 'ramda' //
 import {addDishToCart, removeDishFromCart} from '../actions'
 
 interface IUserState {
-  dishes: any[],
+  dishes: any[]
 }
 
-const initialState: IUserState = {
-  dishes: [
-    {
-      id: 1,
-      name: 'Тартар',
-      price: '120',
-      image: 'https://www.edimdoma.ru/system/images/contents/0000/6787/wide/AdobeStock_275611083_%D0%B8%D1%81%D0%BF%D1%80.jpg?1564142039',
-    },
-    {
-      id: 2,
-      name: 'Тунец',
-      price: '150',
-      image: 'https://www.edimdoma.ru/system/images/contents/0000/6788/wide/AdobeStock_166014726_result.jpeg?1564134468',
-    },
-    {
-      id: 3,
-      name: 'Мидии',
-      price: '90',
-      image: 'https://www.edimdoma.ru/system/images/contents/0000/6789/wide/AdobeStock_196463507_result.jpeg?1564134469',
-    }
-  ],
+export const initialState: IUserState = {
+  dishes: [],
 }
 
 const cartReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(addDishToCart, (state, action) => {
-      return R.append(action.payload, state)
+      const newOBJ = {
+        id: action.payload.id,
+        name: action.payload.name,
+        prise: action.payload.prise,
+        image: action.payload.image,
+      }
+      const stateCopy = {...state}
+      stateCopy.dishes = [...state.dishes]
+      stateCopy.dishes.push(newOBJ)
+      return stateCopy
     })
     .addCase(removeDishFromCart, (state, action) => {
       return R.without(R.of(action.payload), state)
