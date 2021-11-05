6d35325b9fd6823ab90946fb832d511496b6a2ab
Andrei <7656077@mail.ru>
2021-11-05 19:01:07 +0300

    add prifile data

--- src/components/userProfile/personalData.tsx
+++ src/components/userProfile/personalData.tsx
@@ -1,28 +1,52 @@
-
+import {useState, ChangeEvent} from 'react'
 import {UserType} from '../../common/types/userTypes'
 import {AppStoreType} from '../../redux/reducers/rootReducer'
 import {useSelector} from 'react-redux'
+// import {Form, Button} from 'react-bootstrap'
+
+import ShiftingPersonal from
+  './personalData/shiftingPersonal'
+import CompletedPersonal from
+  './personalData/completedPersonal'
+
 
 const PersonalData = () => {
   const user =
     useSelector<AppStoreType, UserType>((state) => state.user)
+
+  const [isShifting, setChangeStatus] = useState<boolean>(true)
+  const changeStatus=(status:boolean)=>{
+    setChangeStatus(status)
+  }
 
   return (
     <div className='profile-block ml-md-auto'>
       <h2 className='profile-subtitle'>
         Личные данные
       </h2>
-      <div className='info-block'>
-        <div className='user-name'>Имя {user.user.name}</div>
-        <div className='second-name'>Фамилия {user.user.secondname}</div>
-        <div className='user-email'>Электронная почта {user.user.email}</div>
-        <div className='phone-number'>Телефон {user.user.name}</div>
-      </div>
-      <button className='btn btn-outline-warning offset-md-10'>
-        Изменить
-      </button>
+      {isShifting ?
+        <CompletedPersonal changeStatus={changeStatus}/> :
+        <ShiftingPersonal changeStatus={changeStatus}/> }
     </div>
   )
 }
 
 export default PersonalData
+
+
+// Добавить адрес: 
+// patch /api/users
+// {
+// city: 'город'
+// street: 'улица'
+// home№: 'дом'
+// homePart: 'корпус'
+// flat: 'квартира'
+// }
+
+// изменить
+
+// удалить
+// {
+// теже поля: пустое значение
+// }
