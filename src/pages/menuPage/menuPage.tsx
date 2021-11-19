24d6a1d7985ecd63cfa58a34a75943f6a932ecba
Anna <anna.vl.gromova@gmail.com>
2021-11-19 21:34:05 +0300

    scroll to top menu page add

--- src/pages/menuPage/menuPage.tsx
+++ src/pages/menuPage/menuPage.tsx
@@ -1,8 +1,12 @@
 
+import ScrollToTop from '../../components/scrollToTop/ScrollToTop'
 import Menu from './Menu/Menu'
 
 const MenuPage = () => (
-  <Menu/>
+  <>
+    <Menu/>
+    <ScrollToTop/>
+  </>
 )
 
 
