b124be8186c09d182fb55a492df03a4650911b23
Polina <polmi713@gmail.com>
2021-11-19 23:49:39 +0300

    add functionality to store cart values for authorized user while refreshing the page

--- src/pages/cartPage/cartPage.tsx
+++ src/pages/cartPage/cartPage.tsx
@@ -1,11 +1,47 @@
+import {useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+import Cookies from 'js-cookie'
+
 import UserCart from '../../components/cartPageComponents/Cart'
-import {DishInCart} from '../../common/types/dishesType'
+
+import {DishInCart, ApiDish} from '../../common/types/dishesType'
+
+import {addDishToCart, clearCart} from '../../redux/actions'
 import {useAppSelector} from '../../redux/hooks'
 
+import {ApiCart} from '../../api/ApiCart'
+
+import {mapApiDishToDishInCart} from '../../utils/typeMappers'
+
 const Cart = () => {
+  const dispatch = useDispatch()
+
+  const isAuthorized: boolean =
+    useAppSelector((state) => state.auth.isAuthorized)
+
   const dishes: DishInCart[] =
     useAppSelector((state) => state.cart.dishes)
-  return <UserCart dishes={dishes} />
+
+  const handleLoad = () => {
+    ApiCart.getCart(Cookies.get('token')).then((resp) => {
+      resp.data.cart.forEach((dish: ApiDish) => {
+        dispatch(
+          addDishToCart(mapApiDishToDishInCart(dish))
+        )
+      })
+    })
+  }
+
+  useEffect(() => {
+    if (isAuthorized) {
+      dispatch(clearCart())
+      handleLoad()
+    }
+  }, [])
+
+  return (
+    <UserCart dishes={dishes} />
+  )
 }
 
 export default Cart
