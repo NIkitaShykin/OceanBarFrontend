c928198bb1393b10dc2be195571ba1d3c13fc61e
Anna <anna.vl.gromova@gmail.com>
2021-11-27 21:25:43 +0300

    tabs for order history & accordion on order add

---
+++ src/components/userProfile/completedOrders.tsx
@@ -0,0 +1,131 @@
+import {Accordion} from 'react-bootstrap'
+
+const CompletedOrders =() => {
+  return (
+    <>
+      {/* {completedOrders.map((order, id) => <Accordion></Accordion>} */}
+      <Accordion
+        // defaultActiveKey='1'
+        flush
+      >
+        <Accordion.Item eventKey='0'>
+          <Accordion.Header>
+            <div className='history-order-title'>
+                Заказ №28 от 28.10.2021г
+            </div>
+          </Accordion.Header>
+          <Accordion.Body>
+            <div className='history-order-block'>
+
+              <div className='shadow border'>
+                <div className='history-order-item'>
+                  <div className='history-header-name'>
+                    Название
+                  </div>
+                  <div className='history-header-quant'>
+                    Количество
+                  </div>
+                  <div className='history-header-price'>
+                    Цена за 1 позицию
+                  </div>
+                </div>
+                <div className='details-devider' />
+                {/* {orderedDishes.map((item, id) => */}
+                <div
+                  className='history-item'>
+                  {/* // id={id)}
+                      // key={item.id}> */}
+                  <div className='details-item-block'>
+                    <span className='details-name'>Суп с лососем</span>
+                  </div>
+                  <div className='details-item-block'>
+                    <span className='counter'>2</span>
+                  </div>
+                  <div className='details-item-block'>
+                    <span>25 BYN</span>
+                  </div>
+                  <div className='details-devider' />
+                </div>
+                {/* )} */}
+
+                <div className='history-order-type-block'>
+                  <div className='row'>
+                    <div className='col order-type-header'>
+                      Тип заказа:
+                    </div>
+                    <div className='col order-type-text'>
+                      Доставка
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='col order-type-header'>
+                      Дата:
+                    </div>
+                    <div className='col order-type-text'>
+                      27.11.2021
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='col order-type-header'>
+                      Время:
+                    </div>
+                    <div className='col order-type-text'>
+                      16.00-17.00
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='col order-type-header'>
+                      Адрес доставки:
+                    </div>
+                    <div className='col order-type-text'>
+                      г.Минск
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='col order-type-header'>
+                      Стол на:
+                    </div>
+                    <div className='col order-type-text'>
+                      на двоих гостей
+                    </div>
+                  </div>
+                </div>
+
+                <div className='details-devider'></div>
+                <div className='row mb-3'>
+                  <div className='col history-total text-right'>
+                    Итого:
+                  </div>
+                  <div className='col history-total'>
+                    25 BYN
+                  </div>
+                </div>
+                <div className='details-devider' />
+                <div className='row'>
+                  <div className='col order-type-header mb'>
+                    Cтатус заказа:
+                  </div>
+                  <div className='col order-type-text mb'>
+                    в процессе
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey='1'>
+          <Accordion.Header>
+            <div className='history-order-title'>
+              Заказ №30 от 19.11.2021г
+            </div>
+          </Accordion.Header>
+          <Accordion.Body>
+            some order
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </>
+  )
+}
+
+export default CompletedOrders
--- src/components/userProfile/orderHistory.tsx
+++ src/components/userProfile/orderHistory.tsx
@@ -1,214 +1,33 @@
-import {useAppSelector} from '../../redux/hooks'
-import {DishInCart} from '../../common/types/dishesType'
+import Tabs from 'react-bootstrap/Tabs'
+import {Tab} from 'react-bootstrap'
+
+import CurrentOrders from './currentOrders'
+import CompletedOrders from './completedOrders'
 
 import '../../pages/profilePage/profile.scss'
 
 
 const orderHistory = () => {
-  const orderedDishes = useAppSelector<DishInCart[]>(
-    (state) => state.cart.dishes
-  )
-
   return (
     <div className='profile-block history ml-md-auto'>
-      <h2>
-        Текущие заказы
-      </h2>
-      <div className='history-order-block'>
-        <div className='history-order-title'>
-         Заказ №36 от 27.11.2021:
-        </div>
-        <div className='shadow border'>
-          <div className='history-order-item'>
-            <div className='history-header-name'>
-             Название
-            </div>
-            <div className='history-header-quant'>
-             Количество
-            </div>
-            <div className='history-header-price'>
-             Цена за 1 позицию
-            </div>
-          </div>
-          <div className='details-devider' />
-          {orderedDishes.map((item, id) =>
-            <div key={item.id}>
-              <div className='history-item' id={String(item.id)} key={item.id}>
-                <div className='details-item-block'>
-                  <span className='details-name'>Суп с лососем</span>
-                </div>
-                <div className='details-item-block'>
-                  <span className='counter'>2</span>
-                </div>
-                <div className='details-item-block'>
-                  <span>25 BYN</span>
-                </div>
-              </div>
-              <div className='details-devider' />
-            </div>
-          )}
-
-          <div className='history-order-type-block'>
-            <div className='row'>
-              <div className='col order-type-header'>
-          Тип заказа:
-              </div>
-              <div className='col order-type-text'>
-          Доставка
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col order-type-header'>
-          Дата:
-              </div>
-              <div className='col order-type-text'>
-               27.11.2021
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col order-type-header'>
-               Время:
-              </div>
-              <div className='col order-type-text'>
-               16.00-17.00
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col order-type-header'>
-                Адрес доставки:
-              </div>
-              <div className='col order-type-text'>
-                г.Минск
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col order-type-header'>
-                Стол на:
-              </div>
-              <div className='col order-type-text'>
-                на двоих гостей
-              </div>
-            </div>
-          </div>
-
-          <div className='details-devider'></div>
-          <div className='row mb-3'>
-            <div className='col history-total text-right'>
-              Итого:
-            </div>
-            <div className='col history-total'>
-              25 BYN
-            </div>
-          </div>
-          <div className='details-devider'/>
-          <div className='row'>
-            <div className='col order-type-header mb'>
-               Cтатус заказа:
-            </div>
-            <div className='col order-type-text mb'>
-               в процессе
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className='history-order-block'>
-        <div className='history-order-title'>
-         Заказ №40 от 28.11.2021:
-        </div>
-        <div className='shadow border'>
-          <div className='history-order-item'>
-            <div className='history-header-name'>
-             Название
-            </div>
-            <div className='history-header-quant'>
-             Количество
-            </div>
-            <div className='history-header-price'>
-             Цена за 1 позицию
-            </div>
-          </div>
-          <div className='details-devider' />
-          {orderedDishes.map((item, id) =>
-            <div key={item.id}>
-              <div className='history-item' id={String(item.id)} key={item.id}>
-                <div className='details-item-block'>
-                  <span className='details-name'>Суп с лососем</span>
-                </div>
-                <div className='details-item-block'>
-                  <span className='counter'>2</span>
-                </div>
-                <div className='details-item-block'>
-                  <span>50 BYN</span>
-                </div>
-              </div>
-              <div className='details-devider' />
-            </div>
-          )}
-
-          <div className='history-order-type-block'>
-            <div className='row'>
-              <div className='col order-type-header'>
-               Тип заказа:
-              </div>
-              <div className='col order-type-text'>
-               Бронирование стола
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col order-type-header'>
-               Дата:
-              </div>
-              <div className='col order-type-text'>
-               28.11.2021
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col order-type-header'>
-               Время:
-              </div>
-              <div className='col order-type-text'>
-               16.00-17.00
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col order-type-header'>
-                Адрес доставки:
-              </div>
-              <div className='col order-type-text'>
-                г.Минск
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col order-type-header'>
-                Стол на:
-              </div>
-              <div className='col order-type-text'>
-                на двоих гостей
-              </div>
-            </div>
-          </div>
-
-          <div className='details-devider'/>
-          <div className='row mb-3'>
-            <div className='col history-total text-right'>
-              Итого:
-            </div>
-            <div className='col history-total'>
-              50 BYN
-            </div>
-          </div>
-          <div className='details-devider'/>
-          <div className='row'>
-            <div className='col order-type-header mb'>
-               Cтатус заказа:
-            </div>
-            <div className='col order-type-text mb'>
-               в процессе
-            </div>
-          </div>
-        </div>
-      </div>
+      <Tabs
+        defaultActiveKey='current'
+        id='uncontrolled-tab'
+        className='mb-3 fs'>
+        <Tab
+          eventKey='current'
+          title='Текущие заказы'
+        >
+          <CurrentOrders />
+        </Tab>
+        <Tab
+          eventKey='history'
+          title='История заказов'
+          className='link-color'
+        >
+          <CompletedOrders />
+        </Tab>
+      </Tabs>
     </div>
   )
 }
