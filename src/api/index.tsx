eb22b4eb25e30e6f8a427d7fcd381219a42d356f
Anna <anna.vl.gromova@gmail.com>
2021-12-02 14:12:34 +0300

    api

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// export const url = 'http://13.51.224.150:3000/api'
-export const url = 'http://localhost:3001/api'
+export const url = 'http://13.51.224.150:3000/api'
 
 export const instance = axios.create({
   baseURL: url,
